af7547da298372e8620159d52166c84e63314cb4
qkrgnsgml <qkrgnsgml1999@gmail.com>
2022-03-16 14:21:58 +0900

    makeProject

---
+++ src/test/java/com/spa/springCommuProject/SpringCommuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spa.springCommuProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommuProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
